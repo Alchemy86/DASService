f996e0d033bcdd15f426fadfca8bab764b70ed37
Aaron Gibson <codebyexample@gmail.com>
2015-05-12 21:54:45 +0100

    Initial Commit 2
    Files added #DASWS-1 Finished

---
+++ AuctionSniperDLL/AuctionSniperDLL/Business/Email.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace AuctionSniperDLL.Business
+{
+    public static class Email
+    {
+        public static void SendEmail(string to,string subject, string message)
+        {
+            try
+            {
+                var mail = new MailMessage("dasservicereport@gmail.com", to);
+                var client = new SmtpClient("smtp.gmail.com", 587)
+                {
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("dasservicereport@gmail.com", "Pr10r1tyF1x")
+                };
+                mail.Subject = subject;
+                mail.Body = message;
+                client.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(@"Failed to send email");
+            }
+
+        }
+    }
+}
